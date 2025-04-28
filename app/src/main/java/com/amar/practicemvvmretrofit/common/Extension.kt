6e570bc1d26cd80c71d8a0310ce6cf3a3c7fa2c0
akshayaamar <akshayaamar05@gmail.com>
2025-04-28 19:07:21 +0530

    extension functions added

---
+++ app/src/main/java/com/amar/practicemvvmretrofit/common/Extension.kt
@@ -0,0 +1,18 @@
+package com.amar.practicemvvmretrofit.common
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+
+fun Context.showToast(
+     message: String,
+     duration: Int = Toast.LENGTH_SHORT
+) = Toast.makeText(this, message, duration).show()
+
+fun View.show() {
+     visibility = View.VISIBLE
+}
+
+fun View.hide() {
+     visibility = View.GONE
+}
--- app/src/main/java/com/amar/practicemvvmretrofit/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/amar/practicemvvmretrofit/ui/fragment/HomeFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.amar.practicemvvmretrofit.R
 import com.amar.practicemvvmretrofit.common.Result
+import com.amar.practicemvvmretrofit.common.hide
+import com.amar.practicemvvmretrofit.common.show
+import com.amar.practicemvvmretrofit.common.showToast
 import com.amar.practicemvvmretrofit.data.model.User
 import com.amar.practicemvvmretrofit.databinding.FragmentHomeBinding
 import com.amar.practicemvvmretrofit.ui.adapter.UserAdapter
@@ -40,24 +43,28 @@ class HomeFragment : Fragment() {
 
           viewmodel.users.observe(viewLifecycleOwner) { result ->
                when (result) {
-                    is Result.Loading -> {
-                         binding.progressBar.visibility = View.VISIBLE
-                         binding.recyclerView.visibility = View.GONE
-                    }
-
-                    is Result.Success -> {
-                         binding.progressBar.visibility = View.GONE
-                         binding.recyclerView.visibility = View.VISIBLE
-                         userAdapter.submitList(result.data.users)
-                    }
-
-                    is Result.Failure -> {
-                         binding.progressBar.visibility = View.GONE
-                         binding.recyclerView.visibility = View.VISIBLE
-                         Toast.makeText(context, result.error?.message ?: "Failure result", Toast.LENGTH_LONG).show()
-                    }
+                    is Result.Loading -> showLoadingIndicator()
+                    is Result.Success -> displayData(result.data.users)
+                    is Result.Failure -> showError(result.error?.message ?: getString(R.string.error_message))
                }
           }
+     }
+
+     private fun showError(message: String) {
+          binding.progressBar.hide()
+          binding.recyclerView.show()
+          requireContext().showToast(message)
+     }
+
+     private fun showLoadingIndicator() {
+          binding.progressBar.show()
+          binding.recyclerView.hide()
+     }
+
+     private fun displayData(users: List<User>?) {
+          binding.progressBar.hide()
+          binding.recyclerView.show()
+          userAdapter.submitList(users)
      }
 
      private fun setUpRecyclerView() {
