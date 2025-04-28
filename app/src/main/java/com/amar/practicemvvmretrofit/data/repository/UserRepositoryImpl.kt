f8d0332302bdf9af795521e19b550edc7c7fe68e
akshayaamar <akshayaamar05@gmail.com>
2025-04-29 02:03:51 +0530

    feat: ResourceProvider utility class added to access string resources

--- app/src/main/java/com/amar/practicemvvmretrofit/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/amar/practicemvvmretrofit/data/repository/UserRepositoryImpl.kt
@@ -3,11 +3,13 @@ package com.amar.practicemvvmretrofit.data.repository
 import com.amar.practicemvvmretrofit.common.Result
 import com.amar.practicemvvmretrofit.data.api.ApiService
 import com.amar.practicemvvmretrofit.data.model.UserData
+import com.amar.practicemvvmretrofit.util.ResourceProvider
 import com.amar.practicemvvmretrofit.util.safeApiCall
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-     private val apiService: ApiService
+     private val apiService: ApiService,
+     private val resourceProvider: ResourceProvider
 ) : UserRepository {
-     override suspend fun getUsers(): Result<UserData> = safeApiCall { apiService.fetchUsers() }
+     override suspend fun getUsers(): Result<UserData> = safeApiCall(resourceProvider) { apiService.fetchUsers() }
 }
--- app/src/main/java/com/amar/practicemvvmretrofit/util/SafeApiCall.kt
+++ app/src/main/java/com/amar/practicemvvmretrofit/util/SafeApiCall.kt
@@ -1,14 +1,20 @@
 package com.amar.practicemvvmretrofit.util
 
 import android.util.Log
+import com.amar.practicemvvmretrofit.R
 import com.amar.practicemvvmretrofit.common.ErrorEntity
+import com.amar.practicemvvmretrofit.common.Result
 import com.amar.practicemvvmretrofit.data.api.ServerEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import retrofit2.Response
-import com.amar.practicemvvmretrofit.common.Result
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 suspend fun <T> safeApiCall(
+     resourceProvider: ResourceProvider,
      apiCall: suspend () -> Response<ServerEntity<T>>
 ): Result<T> = withContext(Dispatchers.IO) {
      try {
@@ -27,16 +33,43 @@ suspend fun <T> safeApiCall(
                               code = error?.code,
                               title = error?.title,
                               subtitle = error?.subtitle,
-                              message = error?.message ?: "Unknown Error"
+                              message = error?.message ?: resourceProvider.getString(R.string.error_unknown)
                          )
                     )
                }
           } else {
-               Log.d("check...repo", "Failure; Not successful")
-               Result.Failure(ErrorEntity(message = response.message() ?: "Something went wrong"))
+               Log.d("check...repo", "Failure; Not successful ${response.message().isEmpty()}")
+               Result.Failure(ErrorEntity(message = response.message().takeIf { it.isNullOrEmpty().not()} ?: resourceProvider.getString(R.string.error_something_went_wrong)))
           }
      } catch (exception: Exception) {
-          Log.d("check...repo", "Failure Exception")
-          Result.Failure(ErrorEntity(message = exception.localizedMessage ?: "Something went wrong"))
+          Log.d("check...repo", "Failure Exception: ${exception.localizedMessage}")
+          Result.Failure(mapExceptionToErrorEntity(exception, resourceProvider))
+     }
+}
+
+private fun mapExceptionToErrorEntity(
+     exception: Exception,
+     resourceProvider: ResourceProvider
+): ErrorEntity {
+     return when (exception) {
+          is UnknownHostException -> {
+               ErrorEntity(message = resourceProvider.getString(R.string.error_no_internet))
+          }
+
+          is SocketTimeoutException -> {
+               ErrorEntity(message = resourceProvider.getString(R.string.error_timeout))
+          }
+
+          is IOException -> {
+               ErrorEntity(message = resourceProvider.getString(R.string.error_io))
+          }
+
+          is HttpException -> {
+               ErrorEntity(message = exception.message() ?: "http: "+resourceProvider.getString(R.string.error_something_went_wrong))
+          }
+
+          else -> {
+               ErrorEntity(message = resourceProvider.getString(R.string.error_unknown))
+          }
      }
 }
